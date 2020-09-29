44924e176f8027b0255ee822b597416b2e36c1b9
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-29 18:20:13 +0300

    добавляет условие 2 задачи

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+const countProps = function (obj) {};
+
+//console.log(countProps({})); // 0
+
+//console.log(countProps({a:1, b:1})); // 2
+
+//console.log(countProps({a:1, b:1, c:1, d:1, e:1})); // 5
