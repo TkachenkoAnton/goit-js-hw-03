5137343859163a94083d33ac621a3d8922e6ca24
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-29 17:27:28 +0300

    добавляет условие 1 задачи

---
+++ js/task-1.js
@@ -0,0 +1,20 @@
+let message = "";
+const user = {
+  age: 20,
+  hobby: "html",
+  name: "Mango",
+  premium: true,
+};
+
+const keys = Object.keys(user);
+// Write code under this line
+
+console.log(message);
+/*
+'age : 20
+hobby : skydiving
+name : Mango
+premium : false
+mood : happy
+"full time" : true
+' */
