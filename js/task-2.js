411bc70083d28bda8d4d22c55dae12557c6006c1
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-29 18:24:36 +0300

    добавляет решение 2 задачи

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,10 @@
-const countProps = function (obj) {};
+const countProps = function (obj) {
+  const objLength = Object.keys(obj);
+  return objLength.length;
+};
 
-//console.log(countProps({})); // 0
+console.log(countProps({})); // 0
 
-//console.log(countProps({a:1, b:1})); // 2
+console.log(countProps({ a: 1, b: 1 })); // 2
 
-//console.log(countProps({a:1, b:1, c:1, d:1, e:1})); // 5
+console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
